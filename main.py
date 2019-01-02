52d65610bf0ed39e98ef27c673db040c16fb3db9
Trish Whetzel <plwhetzel@gmail.com>
2019-01-02 11:51:57 -0800

    add stub schedule page, fix nav header template

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def show_home():
+    ''' Show Home page. '''
     return render_template('index.html')
+
+
+@app.route('/schedule')
+def display_schedule():
+    ''' Show schedule page. '''
+    return render_template('schedule.html')
 
